5b4b548eb6067c4a9ff55179ee27c6d867d0e8a0
Jacob <jacobpayne007@gmail.com>
2024-09-19 19:58:12 -0500

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
+'''We need to join items together'''
+
 def oxford_comma(items):
-    return None
+    if(len(items) > 1):
+        items[-1] = "and " + items[-1]
+    if(len(items)>2):
+        return ", ".join(items)
+    else:
+        return ' '.join(items)
+    
+        
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
